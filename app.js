e1bb4b4f85ac815c57f0a52780306e3897db1e1c
Vitor <vitorzaffari01@gmail.com>
2022-11-07 23:54:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+var valor = $("#value");
+var btn_incrementa = $("#incrementar");
+var btn_decrementa = $("#decrementar");
+var btn_zerar = $("#zerar");
+
+btn_incrementa.click(incrementar);
+
+btn_decrementa.click(decrementar);
+
+btn_zerar.click(zerar);
+
+
+//incremento--------------------------------------------------------------------
+function incrementar() {
+    var contador = parseInt(valor.text());
+    valor.text(contador + 1);
+    adcClasse();
+}
+
+//decremento--------------------------------------------------------------------
+function decrementar() {
+    var contador = parseInt(valor.text());
+    valor.text(contador - 1);
+    console.log(valor.text());
+    adcClasse()
+}
+
+//zerar-------------------------------------------------------------------------
+function zerar() {
+    valor.text(0);
+    console.log(valor.text());
+    adcClasse()
+}
+
+//trocar-cor--------------------------------------------------------------------
+
+function adcClasse() {
+    if (valor.text() > 0) {
+        $("#value").attr("class", "verde");
+    }
+    else if (valor.text() < 0) {
+        $("#value").attr("class", "vermelho");
+    }
+    else {
+        $("#value").removeClass();
+    }
+}
